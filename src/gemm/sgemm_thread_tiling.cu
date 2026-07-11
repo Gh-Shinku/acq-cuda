20da082cf36d47eb17b9087d6db9277dcfe3c7e1
shinku <shinku_1314@qq.com>
2026-07-11 21:26:00 +0800

    [feat] define registers and loop unroll explicitly

--- src/gemm/sgemm_thread_tiling.cu
+++ src/gemm/sgemm_thread_tiling.cu
@@ -10,21 +10,18 @@ namespace gemm {
 namespace {
 
 template <typename T, int BLOCK_TILE_SIZE_X, int BLOCK_TILE_SIZE_Y,
-          int BLOCK_TILE_SIZE_K, int THREAD_TILE_SIZE_X,
-          int THREAD_TILE_SIZE_Y>
-__global__ void sgemm_thread_tiling(int m, int n, int k, T const* A,
-                                    T const* B, T const* C, T* D, T alpha,
-                                    T beta) {
+          int BLOCK_TILE_SIZE_K, int THREAD_TILE_SIZE_X, int THREAD_TILE_SIZE_Y>
+__global__ void sgemm_thread_tiling(int m, int n, int k, T const *A, T const *B,
+                                    T const *C, T *D, T alpha, T beta) {
   static_assert(BLOCK_TILE_SIZE_X % THREAD_TILE_SIZE_X == 0);
   static_assert(BLOCK_TILE_SIZE_Y % THREAD_TILE_SIZE_Y == 0);
 
   constexpr int NUM_THREAD_TILES_X{BLOCK_TILE_SIZE_X / THREAD_TILE_SIZE_X};
   constexpr int NUM_THREAD_TILES_Y{BLOCK_TILE_SIZE_Y / THREAD_TILE_SIZE_Y};
-  constexpr int NUM_THREADS_PER_BLOCK{NUM_THREAD_TILES_X *
-                                      NUM_THREAD_TILES_Y};
+  constexpr int NUM_THREADS_PER_BLOCK{NUM_THREAD_TILES_X * NUM_THREAD_TILES_Y};
 
-  int const thread_linear_idx{static_cast<int>(threadIdx.y * blockDim.x +
-                                               threadIdx.x)};
+  int const thread_linear_idx{
+      static_cast<int>(threadIdx.y * blockDim.x + threadIdx.x)};
 
   __shared__ T A_thread_block_tile[BLOCK_TILE_SIZE_Y][BLOCK_TILE_SIZE_K];
   __shared__ T B_thread_block_tile[BLOCK_TILE_SIZE_K][BLOCK_TILE_SIZE_X];
@@ -33,9 +30,11 @@ __global__ void sgemm_thread_tiling(int m, int n, int k, T const* A,
                                    BLOCK_TILE_SIZE_K};
 
   T C_thread_results[THREAD_TILE_SIZE_Y][THREAD_TILE_SIZE_X]{};
+  T A_thread_regs[THREAD_TILE_SIZE_Y]{};
+  T B_thread_regs[THREAD_TILE_SIZE_X]{};
 
-  for (int thread_block_tile_idx{}; thread_block_tile_idx <
-                                   num_thread_block_tiles;
+  for (int thread_block_tile_idx{};
+       thread_block_tile_idx < num_thread_block_tiles;
        ++thread_block_tile_idx) {
     for (int load_offset{thread_linear_idx};
          load_offset < BLOCK_TILE_SIZE_Y * BLOCK_TILE_SIZE_K;
@@ -76,25 +75,43 @@ __global__ void sgemm_thread_tiling(int m, int n, int k, T const* A,
     }
     __syncthreads();
 
+    /* block 内的数据逻辑坐标 */
+    /* thread_idx_y = thread_linear_idx / NUM_THREAD_TILES_X */
     int const thread_tile_row_base{thread_linear_idx / NUM_THREAD_TILES_X *
                                    THREAD_TILE_SIZE_Y};
+    /* thread_idx_x = `thread_linear_idx % NUM_THREAD_TILES_X` */
     int const thread_tile_col_base{thread_linear_idx % NUM_THREAD_TILES_X *
                                    THREAD_TILE_SIZE_X};
 
+#pragma unroll
     for (int k_i{}; k_i < BLOCK_TILE_SIZE_K; ++k_i) {
+#pragma unroll
       for (int thread_tile_row_idx{}; thread_tile_row_idx < THREAD_TILE_SIZE_Y;
            ++thread_tile_row_idx) {
         int const A_thread_block_tile_row_idx{thread_tile_row_base +
                                               thread_tile_row_idx};
-        T const A_val{A_thread_block_tile[A_thread_block_tile_row_idx][k_i]};
+        A_thread_regs[thread_tile_row_idx] =
+            A_thread_block_tile[A_thread_block_tile_row_idx][k_i];
+      }
 
+#pragma unroll
+      for (int thread_tile_col_idx{}; thread_tile_col_idx < THREAD_TILE_SIZE_X;
+           ++thread_tile_col_idx) {
+        int const B_thread_block_tile_col_idx{thread_tile_col_base +
+                                              thread_tile_col_idx};
+        B_thread_regs[thread_tile_col_idx] =
+            B_thread_block_tile[k_i][B_thread_block_tile_col_idx];
+      }
+
+#pragma unroll
+      for (int thread_tile_row_idx{}; thread_tile_row_idx < THREAD_TILE_SIZE_Y;
+           ++thread_tile_row_idx) {
+#pragma unroll
         for (int thread_tile_col_idx{};
              thread_tile_col_idx < THREAD_TILE_SIZE_X; ++thread_tile_col_idx) {
-          int const B_thread_block_tile_col_idx{thread_tile_col_base +
-                                                thread_tile_col_idx};
-          T const B_val{B_thread_block_tile[k_i][B_thread_block_tile_col_idx]};
           C_thread_results[thread_tile_row_idx][thread_tile_col_idx] +=
-              A_val * B_val;
+              A_thread_regs[thread_tile_row_idx] *
+              B_thread_regs[thread_tile_col_idx];
         }
       }
     }
@@ -127,10 +144,9 @@ __global__ void sgemm_thread_tiling(int m, int n, int k, T const* A,
 }
 
 template <typename T, int BLOCK_TILE_SIZE_X, int BLOCK_TILE_SIZE_Y,
-          int BLOCK_TILE_SIZE_K, int THREAD_TILE_SIZE_X,
-          int THREAD_TILE_SIZE_Y>
-void launch_sgemm_thread_tiling_variant(int m, int n, int k, T const* A,
-                                        T const* B, T const* C, T* D, T alpha,
+          int BLOCK_TILE_SIZE_K, int THREAD_TILE_SIZE_X, int THREAD_TILE_SIZE_Y>
+void launch_sgemm_thread_tiling_variant(int m, int n, int k, T const *A,
+                                        T const *B, T const *C, T *D, T alpha,
                                         T beta, cudaStream_t stream) {
   constexpr int NUM_THREADS_PER_BLOCK{BLOCK_TILE_SIZE_X / THREAD_TILE_SIZE_X *
                                       BLOCK_TILE_SIZE_Y / THREAD_TILE_SIZE_Y};
@@ -138,23 +154,21 @@ void launch_sgemm_thread_tiling_variant(int m, int n, int k, T const* A,
   static_assert(BLOCK_TILE_SIZE_Y % THREAD_TILE_SIZE_Y == 0);
 
   dim3 const block_dim{static_cast<unsigned int>(NUM_THREADS_PER_BLOCK), 1, 1};
-  dim3 const grid_dim{
-      static_cast<unsigned int>((n + BLOCK_TILE_SIZE_X - 1) /
-                                BLOCK_TILE_SIZE_X),
-      static_cast<unsigned int>((m + BLOCK_TILE_SIZE_Y - 1) /
-                                BLOCK_TILE_SIZE_Y),
-      1};
+  dim3 const grid_dim{static_cast<unsigned int>((n + BLOCK_TILE_SIZE_X - 1) /
+                                                BLOCK_TILE_SIZE_X),
+                      static_cast<unsigned int>((m + BLOCK_TILE_SIZE_Y - 1) /
+                                                BLOCK_TILE_SIZE_Y),
+                      1};
 
   sgemm_thread_tiling<T, BLOCK_TILE_SIZE_X, BLOCK_TILE_SIZE_Y,
-                      BLOCK_TILE_SIZE_K, THREAD_TILE_SIZE_X,
-                      THREAD_TILE_SIZE_Y>
+                      BLOCK_TILE_SIZE_K, THREAD_TILE_SIZE_X, THREAD_TILE_SIZE_Y>
       <<<grid_dim, block_dim, 0, stream>>>(m, n, k, A, B, C, D, alpha, beta);
   GEMM_CUDA_CHECK(cudaGetLastError());
 }
 
 template <typename T>
-void launch_sgemm_thread_tiling(int m, int n, int k, T const* A, T const* B,
-                                T const* C, T* D, T alpha, T beta,
+void launch_sgemm_thread_tiling(int m, int n, int k, T const *A, T const *B,
+                                T const *C, T *D, T alpha, T beta,
                                 cudaStream_t stream) {
   int const max_mn{std::max(m, n)};
   if (max_mn <= 256) {
@@ -164,18 +178,18 @@ void launch_sgemm_thread_tiling(int m, int n, int k, T const* A, T const* B,
     launch_sgemm_thread_tiling_variant<T, 32, 32, 16, 2, 2>(
         m, n, k, A, B, C, D, alpha, beta, stream);
   } else {
-    launch_sgemm_thread_tiling_variant<T, 64, 64, 8, 4, 4>(
-        m, n, k, A, B, C, D, alpha, beta, stream);
+    launch_sgemm_thread_tiling_variant<T, 64, 64, 8, 4, 4>(m, n, k, A, B, C, D,
+                                                           alpha, beta, stream);
   }
 }
 
-}  // namespace
+} // namespace
 
-void launch_sgemm_thread_tiling(const SgemmProblem& problem, const float* a,
-                                const float* b, const float* c, float* d,
+void launch_sgemm_thread_tiling(const SgemmProblem &problem, const float *a,
+                                const float *b, const float *c, float *d,
                                 cudaStream_t stream) {
   launch_sgemm_thread_tiling<float>(problem.m, problem.n, problem.k, a, b, c, d,
                                     problem.alpha, problem.beta, stream);
 }
 
-}  // namespace gemm
+} // namespace gemm
